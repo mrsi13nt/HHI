fdfd16701045c6fdadc8ff25515cbd8aaa62f709
mrsi13nt <androgeme0@gmail.com>
2024-02-23 20:44:45 +0200

    fixed some errors in ssl verify

--- func.py
+++ func.py
@@ -1,6 +1,10 @@
 # libs
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import os
+
+# Suppress only the InsecureRequestWarning caused by skipping certificate verification
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 # header to scan
 headers = {
@@ -8,22 +12,25 @@ headers = {
 }
 
 def url(url, head=None, cookies=None):  # main func of scan
-    if head:
-        r = requests.get(url, headers={'Host': head})
-    elif cookies:
-        r = requests.get(url, headers={'Host': 'ping.com',
-        'Cookies': cookies})
-    elif head and cookies:
-        r = requests.get(url, headers={'Host': head,
-        'Cookies': cookies})
-    else:
-        r = requests.get(url, headers=headers)
+    try:
+        if head:
+            r = requests.get(url, headers={'Host': head}, verify=False)
+        elif cookies:
+            r = requests.get(url, headers={'Host': 'ping.com', 'Cookies': cookies}, verify=False)
+        elif head and cookies:
+            r = requests.get(url, headers={'Host': head, 'Cookies': cookies}, verify=False)
+        else:
+            r = requests.get(url, headers=headers, verify=False)
 
-    if r.status_code == 200:
-        print(f'url has host header injection\n {url}')
-    else:
-        print(f'url does not have host header injection\n {url}')
+        if r.status_code == 200:
+            print(f'url has host header injection\n {url}')
+        else:
+            print(f'url does not have host header injection\n {url}')
 
-# checks if the file exist
+    except requests.exceptions.SSLError as e:
+        print(f"SSL Certificate Verification Error: {e}")
+        # Handle SSL certificate verification error as needed
+
+# checks if the file exists
 def check_file_existence(file_path):
     return os.path.exists(file_path)
